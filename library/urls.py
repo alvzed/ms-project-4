5fe2cca1a0f3bc23ba8f5c182ee9acea645e0054
alvzed <alvin.zedendahl@gmail.com>
2020-08-10 14:08:50 +0000

    added video player view and linked up the views to each other

--- library/urls.py
+++ library/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.library, name='library'),
+    path('<video_id>', views.player, name='player')
 ]
--- library/views.py
+++ library/views.py
@@ -1,17 +1,49 @@
-from django.shortcuts import render, redirect
-from .models import Video
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Video, Category
 
 
 # Create your views here.
 def library(request):
     """ a view to to return the landing page """
-    if request.user.is_authenticated:
-        videos = Video.objects.all()
-
-        context = {
-            'videos': videos,
-        }
-
-        return render(request, 'library/library.html', context)
-    else:
+    if not request.user.is_authenticated:
         return redirect('/')
+
+    videos = Video.objects.all()
+    categories = Category.objects.all()
+
+    context = {
+        'videos': videos,
+        'categories': categories,
+    }
+
+    return render(request, 'library/library.html', context)
+
+
+def category(request, category_name):
+    """ a view to to return the landing page """
+    if not request.user.is_authenticated:
+        return redirect('/')
+
+    videos = Video.objects.filter(Category=category_name)
+    categories = Category.objects.all()
+
+    context = {
+        'videos': videos,
+        'categories': categories,
+    }
+
+    return render(request, 'library/category.html', context)
+
+
+def player(request, video_id):
+    """ a view to return the video player for individual videos """
+    if not request.user.is_authenticated:
+        return redirect('/')
+
+    video = get_object_or_404(Video, pk=video_id)
+
+    context = {
+        'video': video,
+    }
+
+    return render(request, 'library/player.html', context)
