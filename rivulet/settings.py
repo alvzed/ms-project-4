d0b3736b743eff3945ffdaae463c8f6c8af102b0
alvzed <alvin.zedendahl@gmail.com>
2020-08-11 08:44:07 +0000

    added media and static file paths

--- rivulet/settings.py
+++ rivulet/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     # apps
     'landing',
     'library',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -153,6 +154,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
