8fd2047920f100e0fec35cd8e1cf1ac3a436be69
alvzed <alvin.zedendahl@gmail.com>
2020-08-13 07:21:40 +0000

    added donate and success views, started adding stripe elements

--- donations/views.py
+++ donations/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect, reverse
+
+import stripe
+
 
 # Create your views here.
+def donate(request):
+    """ a view to to return the landing page """
+
+    user = request.user
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    stripe.public_key = settings.STRIPE_PUBLIC_KEY
+    context = {
+        'user': user,
+        'stripe': stripe,
+    }
+
+    return render(request, 'donations/donate.html', context)
+
+
+def charge(request):
+    amount = 5
+    if request.method == 'POST':
+        print('Data:', request.POST)
+
+    return redirect(reverse('success', args=[amount]))
+
+
+def success(request, args):
+    amount = args
+
+    context = {
+        'amount': amount,
+        'currency': '€',
+    }
+    return render(request, 'donations/success.html', context)
--- rivulet/settings.py
+++ rivulet/settings.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
