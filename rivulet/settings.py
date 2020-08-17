c22cd9bcfe54bc90487ce1c855d8b1fa4cbb885d
alvzed <alvin.zedendahl@gmail.com>
2020-08-17 11:16:35 +0000

    fix: allowed hosts in settings

--- rivulet/settings.py
+++ rivulet/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://rivulet-stream.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['rivulet-stream.herokuapp.com', 'localhost']
 
 
 # Application definition
