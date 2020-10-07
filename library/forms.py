be6a107b180249ede5b2fd437892674ee5f33851
alvzed <alvin.zedendahl@gmail.com>
2020-10-07 13:58:43 +0000

    added some basic bootstrap styling and positioning to the review forms

--- library/forms.py
+++ library/forms.py
@@ -2,8 +2,13 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    rating = forms.IntegerField(min_value=1, max_value=5)
+    rating = forms.IntegerField(min_value=1, max_value=5,
+                                widget=forms.TextInput(attrs={
+                                    'type': 'number',
+                                    'class': 'mx-2 number-form-control'
+                                }))
     description = forms.CharField(max_length=512, required=True,
                                   widget=forms.TextInput(attrs={
-                                      'placeholder': 'Very smart comment...'
+                                      'placeholder': 'Very smart comment...',
+                                      'class': 'form-control mx-2 w-50'
                                   }))
