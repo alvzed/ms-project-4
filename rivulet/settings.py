f85e9ed12d223c7f675cd6fb9b199b0d445fc2db
alvzed <alvin.zedendahl@gmail.com>
2020-10-09 10:20:00 +0000

    set debug to false

--- rivulet/settings.py
+++ rivulet/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['rivulet-stream.herokuapp.com', 'localhost']
